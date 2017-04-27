f1f4fb49cb3aa8f8b14d280c3a7214d9573aae2a
tuyunlei <1213062069@qq.com>
2017-04-27 23:05:21 +0800

    Nothing

--- tgbot.py
+++ tgbot.py
@@ -1,13 +1,17 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler
+#coding:utf-8
+from telegram.ext import Updater, Filters
+from telegram.ext import CommandHandler, MessageHandler, CallbackQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
 import requests, json, logging
 
-updater = Updater(token='Token')
+updater = Updater(token='310866466:AAExdVyQjJ4QETWo6fpJU-KA6aknO6SmUds')
 dispatcher = updater.dispatcher
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
+isInput = False #存储当前bot是否为等待用户输入型号的状态
 
 def start(bot, update):
     keyboard = [[InlineKeyboardButton("LineageOS", callback_data='los'),
@@ -23,9 +27,16 @@ def start(bot, update):
 def button(bot, update):
     query = update.callback_query
 
-    bot.editMessageText(text="Selected ROM: %s" % query.data,
+    bot.editMessageText(text="您选择的ROM是: %s\n请输入并发送您的设备型号" % query.data,
                         chat_id=query.message.chat_id,
                         message_id=query.message.message_id)
+    global selecting
+    selecting = True
+
+def normal(bot, update):
+    if not isInput:
+        text = update.message.text
+        update.message.reply_text("您输入的型号是: %s" % text)
 
 def help(bot, update):
     update.message.reply_text("Use /start to test this bot.")
@@ -33,8 +44,9 @@ def help(bot, update):
     
 
 dispatcher.add_handler(CommandHandler('start', start))
-dispatcher.add_handler(CallbackQueryHandler(button))
 dispatcher.add_handler(CommandHandler('help', help))
+dispatcher.add_handler(MessageHandler(Filters.text, normal))
+dispatcher.add_handler(CallbackQueryHandler(button))
 
 updater.start_polling()
 
