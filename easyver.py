61562867f858ca6987f3c202beb26306b9ffe1cf
unknown <>
2017-05-05 22:16:25 +0800

    To temporarily fix a bug, add another version of the script

---
+++ easyver.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-  
+
+from telegram.ext import Updater, Filters, CommandHandler, MessageHandler
+import requests, json, logging
+
+updater = Updater(token='333123559:AAE0NNoXX7AVs65w3_nKR1CwOemCChjKdmc')
+dispatcher = updater.dispatcher
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+                    
+def start(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text="欢迎来到这里，如果想要获取最新的rom，请输入/help以获取帮助")
+    
+def getrom(bot, update, args):
+    if len(args) == 2:
+        checkDevice(bot, update, args)
+    else :
+        update.message.reply_text("参数有误")
+    
+def checkDevice(bot, update, args):
+    check = requests.get('http://api.termux-cn.tk/%s/%s/check' % (args[1], args[0])).json()
+    if check['msg'] == 'success' :
+        getAPI(bot, update, args)
+    else :
+        update.message.reply_text('参数有误!错误原因：\n' + check['msg'])
+        
+def getAPI(bot, update, args):
+    result = requests.get('http://api.termux-cn.tk/%s/%s/lastest' % (args[1], args[0]))
+    result = result.json()
+    update.message.reply_text("Filename: %s\nSize: %s\nURL: %s\nMD5：%s\nChangelog: %s" % (result['name'], result['size'], result['url'], result['md5'], result['log']))
+
+    
+def help(bot, update):
+    update.message.reply_text("此机器人的使用方式为/getrom 第三方系统名称 机型代号\n目前支持的第三方系统有AICP(aicp) LineageOS(los) Mokee(mokee)\n机型代号请自行查找")
+    
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('getrom', getrom, pass_args=True))
+dispatcher.add_handler(CommandHandler('help', help))
+
+updater.start_polling()
+
+updater.idle()
--- tgbot.py
+++ tgbot.py
@@ -1,54 +1,74 @@
-#coding:utf-8
+# -*- coding:utf-8 -*-  
+
 from telegram.ext import Updater, Filters
-from telegram.ext import CommandHandler, MessageHandler, CallbackQueryHandler
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CommandHandler, MessageHandler
+from telegram import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 
 import requests, json, logging
 
-updater = Updater(token='TOKEN')
+updater = Updater(token='349522201:AAEpHAxrBwcIrRZGT5039PS0gmJ2zSe9x_8')
 dispatcher = updater.dispatcher
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-isInput = False #存储当前bot是否为等待用户输入型号的状态
-
 def start(bot, update):
-    keyboard = [[InlineKeyboardButton("LineageOS", callback_data='los'),
-                 InlineKeyboardButton("Mokee", callback_data='mokee')],
-                
-                [InlineKeyboardButton("AICP", callback_data='aicp')]]
+    # 添加消息处理 handler -> selectRom
+    dispatcher.add_handler(MessageHandler(Filters.text, selectRom))
     
-    reply_markup = InlineKeyboardMarkup(keyboard)
+    # 构建键盘按钮
+    keyboard = [[KeyboardButton("LOS")],
+                [KeyboardButton("Mokee")],
+                [KeyboardButton("AICP")]]
+    
+    reply_markup = ReplyKeyboardMarkup(keyboard, one_time_keyboard='true')
+    
+    update.message.reply_text('Please choose your ROM from below:\nAICP\nLineage OS\nMoKee', reply_markup=reply_markup)
 
-    update.message.reply_text('Please choose your ROM:', reply_markup=reply_markup)
+def selectRom(bot, update):
+    dispatcher.remove_handler(dispatcher.handlers[0][2])
+    # 添加消息处理 -> checkDevice
+    dispatcher.add_handler(MessageHandler(Filters.text, checkDevice))
+    
+    # 删除键盘
+    reply_markup = ReplyKeyboardRemove()
+    
+    # 检查ROM类型
+    if update.message.text.lower() in ['aicp', 'los', 'mokee']:
+        global romType
+        romType = update.message.text.lower()
+        update.message.reply_text("Great, %s selected.\nAnd please tell me your device name" % romType.upper(), reply_markup=reply_markup)
+        
 
-              
-def button(bot, update):
-    query = update.callback_query
+def checkDevice(bot, update):
+    global deviceName
+    deviceName = update.message.text.lower()
+    # 确认设备是否存在
+    check = requests.get('http://api.termux-cn.tk/%s/%s/check' % (deviceName, romType))
+    if check.json()['code'] == 200 :
+        getAPI(bot, update)
+    else :
+        update.message.reply_text(check.json()['msg'])
+    
+    # 取消消息处理
+    dispatcher.remove_handler(dispatcher.handlers[0][2])
 
-    bot.editMessageText(text="您选择的ROM是: %s\n请输入并发送您的设备型号" % query.data,
-                        chat_id=query.message.chat_id,
-                        message_id=query.message.message_id)
-    global selecting
-    selecting = True
-
-def normal(bot, update):
-    if not isInput:
-        text = update.message.text
-        update.message.reply_text("您输入的型号是: %s" % text)
+def getAPI(bot, update):
+    # 构建WEB请求调用API
+    result = requests.get('http://api.termux-cn.tk/%s/%s/lastest' % (deviceName, romType))
+    result = result.json()
+    
+    # 回复用户
+    update.message.reply_text("Filename: %s\nSize: %s\nURL: %s\nMD5：%s\nChangelog: %s" % (result['name'], result['size'], result['url'], result['md5'], result['log']))
 
 def help(bot, update):
-    update.message.reply_text("Use /start to test this bot.")
+    update.message.reply_text("Use /getrom to test this bot.")
 
-    
 
-dispatcher.add_handler(CommandHandler('start', start))
+
+dispatcher.add_handler(CommandHandler('getrom', start))
 dispatcher.add_handler(CommandHandler('help', help))
-dispatcher.add_handler(MessageHandler(Filters.text, normal))
-dispatcher.add_handler(CallbackQueryHandler(button))
 
 updater.start_polling()
 
 updater.idle()
-
